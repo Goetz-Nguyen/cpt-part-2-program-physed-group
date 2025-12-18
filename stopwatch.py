628b625e13640d0d358447cda10f3a225cb6ed3c
aidand08 <341796878@educ.dpcdsb.org>
2025-12-18 13:17:58 -0500

    Add files via upload

---
+++ stopwatch.py
@@ -0,0 +1,15 @@
+import time
+
+input("press enter to start timer")
+start = time.monotonic()
+
+print ("Counting")
+
+input("press enter to start timer")
+end = time.monotonic()
+
+time = end - start
+
+round_time = f"{time:.2f}"
+
+print(f"Time: {round_time}")
